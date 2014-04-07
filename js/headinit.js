b1997c53614b7f2c507413d6df2a814c6ea3ed7a
Aeol Kong <kongsdev@gmail.com>
2014-04-07 22:59:55 +0800

    move local css out from head.load

--- js/headinit.js
+++ js/headinit.js
@@ -1,10 +1,4 @@
-head.load(["/css/normalize.css", 
-	"/css/main.css",
-	"/css/style.css"
-	]);
-
-head.load(["/js/vendor/modernizr-2.6.2.min.js", 
-	"http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
+head.load(["http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
 	"/js/plugins.js",
 	"/js/main.js"
 	]);
