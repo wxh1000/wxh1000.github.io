2378faf8d3eb96777bc63017f6b17ab98b6dfc46
Aeol Kong <kongsdev@gmail.com>
2014-04-08 20:53:57 +0800

    update

--- js/headinit.js
+++ js/headinit.js
@@ -1,6 +1,6 @@
-head.load(["http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
-	"/js/plugins.js",
-	"/js/main.js"
+head.load(["http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+	// "/js/plugins.js",
+	// "/js/main.js"
 	]);
 
        
